894f3a135f16d6da28871a4e0a57034f8e7bc9e1
Florent Le Gall <florent.legall@gmail.com>
2011-11-09 12:16:30 +0100

    Fixed typo

--- concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
+++ concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
@@ -3,116 +3,116 @@ package lobstre.chtrie;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class BasicTrie {
-	static interface MainNode {
-	}
+    static interface MainNode {
+    }
 
-	static interface ArrayNode {
-	}
+    static interface ArrayNode {
+    }
 
-	static class RootNode {
-		public final AtomicReference<INode> root = new AtomicReference<INode> ();
-	}
+    static class RootNode {
+        public final AtomicReference<INode> root = new AtomicReference<INode> ();
+    }
 
-	static class INode implements ArrayNode {
-		public INode(final MainNode n) {
-			this.main = new AtomicReference<MainNode> (n);
-		}
+    static class INode implements ArrayNode {
+        public INode(final MainNode n) {
+            this.main = new AtomicReference<MainNode> (n);
+        }
 
-		public final AtomicReference<MainNode> main;
-	}
+        public final AtomicReference<MainNode> main;
+    }
 
-	static class CNode implements MainNode {
-		CNode(final SNode sNode) {
-			this.array = new ArrayNode[] { sNode };
-			this.bitmap = 1L;
-		}
+    static class CNode implements MainNode {
+        CNode(final SNode sNode) {
+            this.array = new ArrayNode[] { sNode };
+            this.bitmap = 1L;
+        }
 
-		CNode(final ArrayNode[] array, final long bitmap) {
-			this.array = array;
-			this.bitmap = bitmap;
-		}
+        CNode(final ArrayNode[] array, final long bitmap) {
+            this.array = array;
+            this.bitmap = bitmap;
+        }
 
-		public final long bitmap;
-		public final ArrayNode[] array;
-	}
+        public final long bitmap;
+        public final ArrayNode[] array;
+    }
 
-	static class SNode implements MainNode, ArrayNode {
-		SNode(final Object k, final Object v, final boolean tomb) {
-			this.key = k;
-			this.value = v;
-			this.tomb = tomb;
-		}
+    static class SNode implements MainNode, ArrayNode {
+        SNode(final Object k, final Object v, final boolean tomb) {
+            this.key = k;
+            this.value = v;
+            this.tomb = tomb;
+        }
 
-		public final Object key;
-		public final Object value;
-		public final boolean tomb;
-	}
+        public final Object key;
+        public final Object value;
+        public final boolean tomb;
+    }
 
-	static class FlagPos {
-		FlagPos(final long flag, final int position) {
-			this.flag = flag;
-			this.position = position;
-		}
+    static class FlagPos {
+        FlagPos(final long flag, final int position) {
+            this.flag = flag;
+            this.position = position;
+        }
 
-		public final long flag;
-		public final int position;
-	}
+        public final long flag;
+        public final int position;
+    }
 
-	public static void insert (final RootNode root, final int width, final Object k, final Object v) {
-		final INode r = root.root.get ();
-		if (r == null || isNullInode (r)) {
-			final CNode cn = new CNode (new SNode (k, v, false));
-			final INode nr = new INode (cn);
-			if (!root.root.compareAndSet (r, nr)) {
-				insert (root, width, k, v);
-			}
-		} else if (!iinsert (width, r, k, v, 0, null)) {
-			insert (root, width, k, v);
-		}
-	}
+    public static void insert (final RootNode root, final int width, final Object k, final Object v) {
+        final INode r = root.root.get ();
+        if (r == null || isNullInode (r)) {
+            final CNode cn = new CNode (new SNode (k, v, false));
+            final INode nr = new INode (cn);
+            if (!root.root.compareAndSet (r, nr)) {
+                insert (root, width, k, v);
+            }
+        } else if (!iinsert (width, r, k, v, 0, null)) {
+            insert (root, width, k, v);
+        }
+    }
 
-	private static boolean iinsert (final int width, final INode i, final Object k, final Object v, final int level, final Object parent) {
-		final MainNode main = i.main.get ();
-		if (main instanceof CNode) {
-			final CNode cn = (CNode) main;
-			final FlagPos flagPos = flagPos (k.hashCode (), level, cn.bitmap, width);
-			if (0 == (flagPos.flag & cn.bitmap)) {
-				final SNode snode = new SNode (k, v, false);
-				final ArrayNode[] narr = inserted (cn.array, flagPos.position, snode);
-				final CNode ncn = new CNode (narr, flagPos.flag | cn.bitmap);
-				return i.main.compareAndSet (main, ncn);
-			}
-		}
-		return false;
-	}
+    private static boolean iinsert (final int width, final INode i, final Object k, final Object v, final int level, final Object parent) {
+        final MainNode main = i.main.get ();
+        if (main instanceof CNode) {
+            final CNode cn = (CNode) main;
+            final FlagPos flagPos = flagPos (k.hashCode (), level, cn.bitmap, width);
+            if (0 == (flagPos.flag & cn.bitmap)) {
+                final SNode snode = new SNode (k, v, false);
+                final ArrayNode[] narr = inserted (cn.array, flagPos.position, snode);
+                final CNode ncn = new CNode (narr, flagPos.flag | cn.bitmap);
+                return i.main.compareAndSet (main, ncn);
+            }
+        }
+        return false;
+    }
 
-	private static ArrayNode[] inserted (final ArrayNode[] array, final int position, final SNode snode) {
-		final ArrayNode[] narr = new ArrayNode[array.length + 1];
-		for (int i = 0; i < array.length + 1; i++) {
-			if (i < position) {
-				narr [i] = array [i];
-			} else if (i == position) {
-				narr [i] = snode;
-			} else {
-				narr [i] = array [i + 1];
-			}
-		}
-		return narr;
-	}
+    private static ArrayNode[] inserted (final ArrayNode[] array, final int position, final SNode snode) {
+        final ArrayNode[] narr = new ArrayNode[array.length + 1];
+        for (int i = 0; i < array.length + 1; i++) {
+            if (i < position) {
+                narr [i] = array [i];
+            } else if (i == position) {
+                narr [i] = snode;
+            } else {
+                narr [i] = array [i + 1];
+            }
+        }
+        return narr;
+    }
 
-	static boolean isNullInode (final INode r) {
-		return r.main.get () != null;
-	}
+    static boolean isNullInode (final INode r) {
+        return r.main.get () != null;
+    }
 
-	static FlagPos flagPos (final int hc, final int level, final long bitmap, final int w) {
-		final int bitsRemaining = Math.min (w, 32 - level);
-		final int subHash = hc >> level & (1 << bitsRemaining) - 1;
-		final long flag = 1L << subHash;
-		final int pos = Long.bitCount (flag - 1 & bitmap);
-		return new FlagPos (flag, pos);
-	}
+    static FlagPos flagPos (final int hc, final int level, final long bitmap, final int w) {
+        final int bitsRemaining = Math.min (w, 32 - level);
+        final int subHash = hc >> level & (1 << bitsRemaining) - 1;
+        final long flag = 1L << subHash;
+        final int pos = Long.bitCount (flag - 1 & bitmap);
+        return new FlagPos (flag, pos);
+    }
 
-	static long bitCount (final long v) {
-		return Long.bitCount (v);
-	}
+    static long bitCount (final long v) {
+        return Long.bitCount (v);
+    }
 }
--- concurrent-hash-trie/test/lobstre/chtrie/TestBasicTrie.java
+++ concurrent-hash-trie/test/lobstre/chtrie/TestBasicTrie.java
@@ -1,44 +1,43 @@
 package lobstre.chtrie;
 
-
 public class TestBasicTrie {
     public static void main (String[] args) {
         assertEquals (1L, BasicTrie.flagPos (0, 0, 0L, 6).flag);
         assertEquals (0, BasicTrie.flagPos (0, 0, 0L, 6).position);
-        
+
         assertEquals (2L, BasicTrie.flagPos (1, 0, 0L, 6).flag);
         assertEquals (0, BasicTrie.flagPos (1, 0, 0L, 6).position);
-        
+
         assertEquals (1L, BasicTrie.flagPos (0, 0, 1L, 6).flag);
         assertEquals (0, BasicTrie.flagPos (0, 0, 1L, 6).position);
-        
+
         assertEquals (2L, BasicTrie.flagPos (1, 0, 1L, 6).flag);
         assertEquals (1, BasicTrie.flagPos (1, 0, 1L, 6).position);
-        
+
         assertEquals (4L, BasicTrie.flagPos (2, 0, 1L, 6).flag);
         assertEquals (1, BasicTrie.flagPos (2, 0, 1L, 6).position);
-        
+
         assertEquals (32L, BasicTrie.flagPos (5, 0, 0L, 6).flag);
         assertEquals (0, BasicTrie.flagPos (5, 0, 0L, 6).position);
-        
+
         assertEquals (32L, BasicTrie.flagPos (5, 0, 2L, 6).flag);
         assertEquals (1, BasicTrie.flagPos (5, 0, 2L, 6).position);
-        
+
         assertEquals (32L, BasicTrie.flagPos (5, 0, 2L, 6).flag);
         assertEquals (1, BasicTrie.flagPos (5, 0, 2L, 6).position);
-        
+
         assertEquals (4L, BasicTrie.flagPos (2, 0, 2L, 6).flag);
         assertEquals (1, BasicTrie.flagPos (2, 0, 2L, 6).position);
-        
+
         assertEquals (16L, BasicTrie.flagPos (4, 0, 2L, 6).flag);
         assertEquals (1, BasicTrie.flagPos (4, 0, 2L, 6).position);
-        
+
         assertEquals (2L, BasicTrie.flagPos (64 + 4, 6, 2L, 6).flag);
         assertEquals (0, BasicTrie.flagPos (64 + 4, 6, 2L, 6).position);
-        
+
         assertEquals (16L, BasicTrie.flagPos (256 + 4, 6, 3L, 6).flag);
         assertEquals (2, BasicTrie.flagPos (256 + 4, 6, 3L, 6).position);
-        
+
         assertEquals (Long.MIN_VALUE, BasicTrie.flagPos (-1, 6, 63, 6).flag);
         assertEquals (6, BasicTrie.flagPos (-1, 6, 63, 6).position);
     }
@@ -48,7 +47,7 @@ public class TestBasicTrie {
             new Exception ("Error : expected: " + expected + " found: " + found).printStackTrace ();
         }
     }
-    
+
     private static void assertEquals (int expected, int found) {
         if (expected != found) {
             new Exception ("Error : expected: " + expected + " found: " + found).printStackTrace ();
