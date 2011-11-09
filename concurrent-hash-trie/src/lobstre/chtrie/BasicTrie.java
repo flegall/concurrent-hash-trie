a3991f7844a1647da45cb6ad7446525c16409b9a
Florent Le Gall <florent.legall@gmail.com>
2011-11-09 22:08:14 +0100

    Made root & width instance variables, removed static methods.

--- concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
+++ concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
@@ -3,75 +3,28 @@ package lobstre.chtrie;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class BasicTrie {
-    static interface MainNode {
+    private final int width;
+    private final AtomicReference<INode> root;
+
+    public BasicTrie() {
+        this.width = 6;
+        this.root = new AtomicReference<INode> ();
     }
 
-    static interface ArrayNode {
-    }
-
-    static class RootNode {
-        public final AtomicReference<INode> root = new AtomicReference<INode> ();
-    }
-
-    static class INode implements ArrayNode {
-        public INode(final MainNode n) {
-            this.main = new AtomicReference<MainNode> (n);
-        }
-
-        public final AtomicReference<MainNode> main;
-    }
-
-    static class CNode implements MainNode {
-        CNode(final SNode sNode) {
-            this.array = new ArrayNode[] { sNode };
-            this.bitmap = 1L;
-        }
-
-        CNode(final ArrayNode[] array, final long bitmap) {
-            this.array = array;
-            this.bitmap = bitmap;
-        }
-
-        public final long bitmap;
-        public final ArrayNode[] array;
-    }
-
-    static class SNode implements MainNode, ArrayNode {
-        SNode(final Object k, final Object v, final boolean tomb) {
-            this.key = k;
-            this.value = v;
-            this.tomb = tomb;
-        }
-
-        public final Object key;
-        public final Object value;
-        public final boolean tomb;
-    }
-
-    static class FlagPos {
-        FlagPos(final long flag, final int position) {
-            this.flag = flag;
-            this.position = position;
-        }
-
-        public final long flag;
-        public final int position;
-    }
-
-    public static void insert (final RootNode root, final int width, final Object k, final Object v) {
-        final INode r = root.root.get ();
+    public void insert (final Object k, final Object v) {
+        final INode r = root.get ();
         if (r == null || isNullInode (r)) {
             final CNode cn = new CNode (new SNode (k, v, false));
             final INode nr = new INode (cn);
-            if (!root.root.compareAndSet (r, nr)) {
-                insert (root, width, k, v);
+            if (!root.compareAndSet (r, nr)) {
+                insert (k, v);
             }
-        } else if (!iinsert (width, r, k, v, 0, null)) {
-            insert (root, width, k, v);
+        } else if (!iinsert (r, k, v, 0, null)) {
+            insert (k, v);
         }
     }
 
-    private static boolean iinsert (final int width, final INode i, final Object k, final Object v, final int level, final INode parent) {
+    private boolean iinsert (final INode i, final Object k, final Object v, final int level, final INode parent) {
         final MainNode main = i.main.get ();
         if (main instanceof CNode) {
             final CNode cn = (CNode) main;
@@ -85,7 +38,7 @@ public class BasicTrie {
             final ArrayNode an = cn.array [flagPos.position];
             if (an instanceof INode) {
                 final INode in = (INode) an;
-                return iinsert (width, in, k, v, level + width, i);
+                return iinsert (in, k, v, level + width, i);
             }
             if (an instanceof SNode && !((SNode) an).tomb) {
                 final SNode sn = (SNode) an;
@@ -96,7 +49,7 @@ public class BasicTrie {
                     return i.main.compareAndSet (main, ncn);
                 } else {
                     final FlagPos sfp = flagPos (k.hashCode (), level + width, 0L, width);
-                    final CNode scn = new CNode (new ArrayNode [] {nsn}, sfp.flag);
+                    final CNode scn = new CNode (new ArrayNode[] { nsn }, sfp.flag);
                     final INode nin = new INode (scn);
                     final ArrayNode[] narr = updated (cn.array, flagPos.position, nin);
                     final CNode ncn = new CNode (narr, cn.bitmap);
@@ -113,7 +66,7 @@ public class BasicTrie {
         return false;
     }
 
-    private static void clean (INode parent) {
+    private void clean (INode parent) {
         // TODO
     }
 
@@ -155,7 +108,54 @@ public class BasicTrie {
         return new FlagPos (flag, pos);
     }
 
-    static long bitCount (final long v) {
-        return Long.bitCount (v);
+    static interface MainNode {
+    }
+
+    static interface ArrayNode {
+    }
+
+    static class INode implements ArrayNode {
+        public INode(final MainNode n) {
+            this.main = new AtomicReference<MainNode> (n);
+        }
+    
+        public final AtomicReference<MainNode> main;
+    }
+
+    static class CNode implements MainNode {
+        CNode(final SNode sNode) {
+            this.array = new ArrayNode[] { sNode };
+            this.bitmap = 1L;
+        }
+    
+        CNode(final ArrayNode[] array, final long bitmap) {
+            this.array = array;
+            this.bitmap = bitmap;
+        }
+    
+        public final long bitmap;
+        public final ArrayNode[] array;
+    }
+
+    static class SNode implements MainNode, ArrayNode {
+        SNode(final Object k, final Object v, final boolean tomb) {
+            this.key = k;
+            this.value = v;
+            this.tomb = tomb;
+        }
+    
+        public final Object key;
+        public final Object value;
+        public final boolean tomb;
+    }
+
+    static class FlagPos {
+        FlagPos(final long flag, final int position) {
+            this.flag = flag;
+            this.position = position;
+        }
+    
+        public final long flag;
+        public final int position;
     }
 }
