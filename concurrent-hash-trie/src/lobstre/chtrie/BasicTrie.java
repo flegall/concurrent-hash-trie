8fa27e7fb85e703c93619e4a717377b5916451a6
Florent Le Gall <florent.legall@gmail.com>
2011-11-10 15:01:22 +0100

    Implemented lookup

--- concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
+++ concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
@@ -6,7 +6,7 @@ public class BasicTrie {
     private final int width;
     private final AtomicReference<INode> root;
 
-    public BasicTrie() {
+    public BasicTrie () {
         this.width = 6;
         this.root = new AtomicReference<INode> ();
     }
@@ -26,6 +26,59 @@ public class BasicTrie {
         }
     }
 
+    public Object lookup (final Object k) {
+        final INode r = this.root.get ();
+        if (null == r) {
+            return null;
+        } else if (isNullInode (r)) {
+            this.root.compareAndSet (r, null);
+            return lookup (k);
+        } else {
+            Result res = ilookup (r, k, 0, null);
+            switch (res.type) {
+            case FOUND:
+                return res.result;
+            case NOTFOUND:
+                return null;
+            case RESTART:
+                return lookup (k);
+            default:
+                throw new RuntimeException ("Unexpected case: "+res.type);
+            }
+        }
+    }
+
+    private Result ilookup (INode i, Object k, int level, INode parent) {
+        final MainNode main = i.main.get ();
+        if (main instanceof CNode) {
+            final CNode cn = (CNode) main;
+            final FlagPos flagPos = flagPos (k.hashCode (), level, cn.bitmap, this.width);
+            if (0 == (flagPos.flag & cn.bitmap)) {
+                return new Result (ResultType.NOTFOUND, null);    
+            }
+            final ArrayNode an = cn.array [flagPos.position];
+            if (an instanceof INode) {
+                final INode sin = (INode) an;
+                return ilookup (sin, k, level + width, i);
+            }
+            if (an instanceof SNode && !((SNode) an).tomb) {
+                final SNode sn = (SNode) an;
+                if (sn.key.equals (k)) {
+                    return new Result (ResultType.FOUND, sn.value);
+                } else {
+                    return new Result (ResultType.NOTFOUND, null);    
+                }
+            }
+        }
+        if ((main instanceof SNode && ((SNode)main).tomb) || main == null) {
+            if (parent != null) {
+                clean (parent);
+            }
+            return new Result (ResultType.RESTART, null);
+        }
+        throw new RuntimeException ("Found CNODE/SNODE.tomb!");
+    }
+
     private boolean iinsert (final INode i, final Object k, final Object v, final int level, final INode parent) {
         final MainNode main = i.main.get ();
 
@@ -39,8 +92,8 @@ public class BasicTrie {
             }
             final ArrayNode an = cn.array [flagPos.position];
             if (an instanceof INode) {
-                final INode in = (INode) an;
-                return iinsert (in, k, v, level + this.width, i);
+                final INode sin = (INode) an;
+                return iinsert (sin, k, v, level + this.width, i);
             }
             if (an instanceof SNode && !((SNode) an).tomb) {
                 final SNode sn = (SNode) an;
@@ -57,7 +110,7 @@ public class BasicTrie {
                 }
             }
         }
-        if (main instanceof SNode || main == null) {
+        if ((main instanceof SNode && ((SNode)main).tomb) || main == null) {
             if (parent != null) {
                 clean (parent);
             }
@@ -120,22 +173,37 @@ public class BasicTrie {
     }
 
     static class INode implements ArrayNode {
-        public INode(final MainNode n) {
+        public INode (final MainNode n) {
             this.main = new AtomicReference<MainNode> (n);
         }
 
         public final AtomicReference<MainNode> main;
     }
 
+    static enum ResultType {
+        FOUND,
+        NOTFOUND,
+        RESTART
+    }
+
+    static class Result {
+        public Result (final ResultType type, final Object result) {
+            this.type = type;
+            this.result = result;
+        }
+
+        public final Object result;
+        public final ResultType type;
+    }
+
     static class CNode implements MainNode {
-        CNode(final SNode sNode, final int width) {
+        CNode (final SNode sNode, final int width) {
             final long flag = BasicTrie.flag (sNode.key.hashCode (), 0, width);
             this.array = new ArrayNode[] { sNode };
             this.bitmap = flag;
         }
 
-
-        CNode(final SNode sn1, final SNode sn2, final int level, final int width) {
+        CNode (final SNode sn1, final SNode sn2, final int level, final int width) {
             final long flag1 = BasicTrie.flag (sn1.key.hashCode (), level, width);
             final long flag2 = BasicTrie.flag (sn2.key.hashCode (), level, width);
             if (flag1 < flag2) {
@@ -145,8 +213,8 @@ public class BasicTrie {
             }
             this.bitmap = flag1 | flag2;
         }
-        
-        public CNode inserted (FlagPos flagPos, SNode snode) {
+
+        public CNode inserted (final FlagPos flagPos, final SNode snode) {
             final ArrayNode[] narr = BasicTrie.inserted (this.array, flagPos.position, snode);
             return new CNode (narr, flagPos.flag | this.bitmap);
         }
@@ -156,7 +224,7 @@ public class BasicTrie {
             return new CNode (narr, this.bitmap);
         }
 
-        CNode(final ArrayNode[] array, final long bitmap) {
+        CNode (final ArrayNode[] array, final long bitmap) {
             this.array = array;
             this.bitmap = bitmap;
         }
@@ -166,7 +234,7 @@ public class BasicTrie {
     }
 
     static class SNode implements MainNode, ArrayNode {
-        SNode(final Object k, final Object v, final boolean tomb) {
+        SNode (final Object k, final Object v, final boolean tomb) {
             this.key = k;
             this.value = v;
             this.tomb = tomb;
@@ -178,7 +246,7 @@ public class BasicTrie {
     }
 
     static class FlagPos {
-        FlagPos(final long flag, final int position) {
+        FlagPos (final long flag, final int position) {
             this.flag = flag;
             this.position = position;
         }
