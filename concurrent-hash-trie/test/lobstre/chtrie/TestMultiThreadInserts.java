3112bbcfecc3bbe607b249205b819b0c1d0f6302
Florent Le Gall <florent.legall@gmail.com>
2011-11-20 22:41:53 +0100

    Added multi-threads insertion test.

--- concurrent-hash-trie/test/lobstre/chtrie/TestMultiThreadInserts.java
+++ concurrent-hash-trie/test/lobstre/chtrie/TestMultiThreadInserts.java
@@ -4,18 +4,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class TestMultiThread {
+public class TestMultiThreadInserts {
     public static void main (final String[] args) {
-        int nThreads = 2;
+        final int nThreads = 2;
         final ExecutorService es = Executors.newFixedThreadPool (nThreads);
         final BasicTrie bt = new BasicTrie ();
         for (int i = 0; i < nThreads; i++) {
+            final int threadNo = i;
             es.execute (new Runnable () {
                 @Override
                 public void run () {
                     for (int j = 0; j < 500 * 1000; j++) {
-                        bt.insert (Integer.valueOf (j), Integer.valueOf (j));
-                        bt.remove (Integer.valueOf (j));
+                        if (j % nThreads == threadNo) {
+                            bt.insert (Integer.valueOf (j), Integer.valueOf (j));
+                        }
                     }
                 }
             });
@@ -27,6 +29,10 @@ public class TestMultiThread {
         } catch (final InterruptedException e) {
             e.printStackTrace ();
         }
-        ;
+        
+        for (int j = 0; j < 500 * 1000; j++) {
+            final Object lookup = bt.lookup (Integer.valueOf (j));
+            TestHelper.assertEquals (Integer.valueOf (j), lookup);
+        }
     }
 }
