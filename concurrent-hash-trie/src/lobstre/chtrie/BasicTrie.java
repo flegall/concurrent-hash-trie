645174ee205468e6330d190a480e9f64592452ec
Florent Le Gall <florent.legall@gmail.com>
2011-11-25 13:48:19 +0100

    Removed unused hash method.

--- concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
+++ concurrent-hash-trie/src/lobstre/chtrie/BasicTrie.java
@@ -333,14 +333,6 @@ public class BasicTrie {
         h ^= (h >>> 20) ^ (h >>> 12);
         return h ^ (h >>> 7) ^ (h >>> 4);
     }
-    
-    static int hash(int h) {
-        // This function ensures that hashCodes that differ only by
-        // constant multiples at each bit position have a bounded
-        // number of collisions (approximately 8 at default load factor).
-        h ^= (h >>> 20) ^ (h >>> 12);
-        return h ^ (h >>> 7) ^ (h >>> 4);
-    }
 
     /**
      * Returns a copy an {@link BranchNode} array with an updated
