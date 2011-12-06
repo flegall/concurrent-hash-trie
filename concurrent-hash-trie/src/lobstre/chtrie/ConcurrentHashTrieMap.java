8d0df15ddc38d799c92123a93ba8b205ecb74f19
Florent Le Gall <florent.legall@gmail.com>
2011-12-06 21:16:57 +0100

    Removed iterator implementation first try.

--- concurrent-hash-trie/src/lobstre/chtrie/ConcurrentHashTrieMap.java
+++ concurrent-hash-trie/src/lobstre/chtrie/ConcurrentHashTrieMap.java
@@ -113,42 +113,18 @@ public class ConcurrentHashTrieMap<K, V> extends AbstractMap<K, V> {
     }
 
     private final class Iter implements Iterator<Map.Entry<K, V>> {
-        private KeyValueNode<K, V> current = null;
-        private KeyValueNode<K, V> next = null;
         
         public Iter () {
-            current = lookupNext (null);
         }
         
         @Override
         public boolean hasNext () {
-            return next != null;
+            return false;
         }
 
         @Override
         public Entry<K, V> next () {
-            current = next;
-            next = lookupNext (current);
-            return new Entry<K, V>() {
-                private final K key = current.key;
-                private V value = current.value;
-                @Override
-                public K getKey () {
-                    return key;
-                }
-
-                @Override
-                public V getValue () {
-                    return value;
-                }
-
-                @Override
-                public V setValue (V v) {
-                    final V put = put (key, v);
-                    this.value = put;
-                    return this.value;
-                }
-            };
+            return null;
         }
 
         @Override
@@ -276,41 +252,41 @@ public class ConcurrentHashTrieMap<K, V> extends AbstractMap<K, V> {
         }
     }
     
-    KeyValueNode<K, V> lookupNext (final KeyValueNode<K, V> kvn) {
-        if (kvn != null) {
-            notNullKey (kvn.key);
-            final int hc = hash (kvn.key);
-            while (true) {
-                // Getting lookup result
-                final Result<KeyValueNode<K, V>> res = ilookupNext (this.root, hc, kvn, 0, null);
-                switch (res.type) {
-                case FOUND:
-                    return res.result;
-                case NOTFOUND:
-                    return null;
-                case RESTART:
-                    continue;
-                default:
-                    throw new RuntimeException ("Unexpected case: " + res.type);
-                }
-            }
-        } else {
-            while (true) {
-                // Getting lookup result
-                final Result<KeyValueNode<K, V>> res = ilookupFirst (this.root, 0, null);
-                switch (res.type) {
-                case FOUND:
-                    return res.result;
-                case NOTFOUND:
-                    return null;
-                case RESTART:
-                    continue;
-                default:
-                    throw new RuntimeException ("Unexpected case: " + res.type);
-                }
-            }
-        }
-    }
+//    KeyValueNode<K, V> lookupNext (final KeyValueNode<K, V> kvn) {
+//        if (kvn != null) {
+//            notNullKey (kvn.key);
+//            final int hc = hash (kvn.key);
+//            while (true) {
+//                // Getting lookup result
+//                final Result<KeyValueNode<K, V>> res = ilookupNext (this.root, hc, kvn, 0, null);
+//                switch (res.type) {
+//                case FOUND:
+//                    return res.result;
+//                case NOTFOUND:
+//                    return null;
+//                case RESTART:
+//                    continue;
+//                default:
+//                    throw new RuntimeException ("Unexpected case: " + res.type);
+//                }
+//            }
+//        } else {
+//            while (true) {
+//                // Getting lookup result
+//                final Result<KeyValueNode<K, V>> res = ilookupFirst (this.root, 0, null);
+//                switch (res.type) {
+//                case FOUND:
+//                    return res.result;
+//                case NOTFOUND:
+//                    return null;
+//                case RESTART:
+//                    continue;
+//                default:
+//                    throw new RuntimeException ("Unexpected case: " + res.type);
+//                }
+//            }
+//        }
+//    }
 
     private Result<V> ilookup (final INode i, final int hashcode, final K k, final int level, final INode parent) {
         final MainNode main = i.getMain ();
@@ -483,88 +459,6 @@ public class ConcurrentHashTrieMap<K, V> extends AbstractMap<K, V> {
         if (main instanceof TNode) {
             clean (parent, level - this.width);
             return new Result<V> (ResultType.RESTART, null);
-        }
-        throw new RuntimeException ("Unexpected case: " + main);
-    }
-
-    private Result<KeyValueNode<K, V>> ilookupFirst (final INode i, final int level, final INode parent) {
-        final MainNode main = i.getMain ();
-
-        // Usual case
-        if (main instanceof CNode) {
-            @SuppressWarnings("unchecked")
-            final CNode<K, V> cn = (CNode<K, V>) main;
-            if (cn.bitmap == 0L) {
-                return new Result<KeyValueNode<K, V>> (ResultType.NOTFOUND, null);
-            }
-            final BranchNode an = cn.array [0];
-            if (an instanceof INode) {
-                // Looking down
-                final INode sin = (INode) an;
-                return ilookupFirst (sin, level + this.width, i);
-            }
-            if (an instanceof SNode) {
-                // Found the hash locally, let's see if it matches
-                @SuppressWarnings("unchecked")
-                final SNode<K, V> sn = (SNode<K, V>) an;
-                final KeyValueNode<K, V> v = sn.first ();
-                if (null != v) {
-                    return new Result<KeyValueNode<K, V>> (ResultType.FOUND, v);
-                } else {
-                    return new Result<KeyValueNode<K, V>> (ResultType.NOTFOUND, null);
-                }
-            }
-        }
-
-        // Cleaning up trie
-        if (main instanceof TNode) {
-            clean (parent, level - this.width);
-            return new Result<KeyValueNode<K, V>> (ResultType.RESTART, null);
-        }
-        throw new RuntimeException ("Unexpected case: " + main);
-    }
-
-    private Result<KeyValueNode<K, V>> ilookupNext (final INode i, final int hashcode, final KeyValueNode<K, V> kvn, final int level, final INode parent) {
-        final MainNode main = i.getMain ();
-
-        // Usual case
-        if (main instanceof CNode) {
-            @SuppressWarnings("unchecked")
-            final CNode<K, V> cn = (CNode<K, V>) main;
-            final FlagPos flagPos = flagPos (hashcode, level, cn.bitmap, this.width);
-
-            // Asked for a hash not in trie
-            if (0L == (flagPos.flag & cn.bitmap)) {
-                return new Result<KeyValueNode<K, V>> (ResultType.NOTFOUND, null);
-            }
-
-            final BranchNode an = cn.array [flagPos.position];
-            if (an instanceof INode) {
-                // Looking down
-                final INode sin = (INode) an;
-                return ilookupNext (sin, hashcode, kvn, level + this.width, i);
-            }
-            if (an instanceof SNode) {
-                // Found the hash locally, let's see if it matches
-                @SuppressWarnings("unchecked")
-                final SNode<K, V> sn = (SNode<K, V>) an;
-                if (sn.hash () == hashcode) {
-                    final V v = sn.get (kvn.key);
-                    if (null != v) {
-                        return new Result<KeyValueNode<K, V>> (ResultType.FOUND, new KeyValueNode<K, V> (kvn.key, v));
-                    } else {
-                        return new Result<KeyValueNode<K, V>> (ResultType.NOTFOUND, null);
-                    }
-                } else {
-                    return new Result<KeyValueNode<K, V>> (ResultType.NOTFOUND, null);
-                }
-            }
-        }
-
-        // Cleaning up trie
-        if (main instanceof TNode) {
-            clean (parent, level - this.width);
-            return new Result<KeyValueNode<K, V>> (ResultType.RESTART, null);
         }
         throw new RuntimeException ("Unexpected case: " + main);
     }
@@ -807,12 +701,6 @@ public class ConcurrentHashTrieMap<K, V> extends AbstractMap<K, V> {
          * @return the hashcode
          */
         int hash ();
-
-        /**
-         * Gets the first value contained in this {@link SNode}
-         * @return the first value
-         */
-        KeyValueNode<K, V> first ();
 
         /**
          * Gets an Object associated with the given key
@@ -1105,11 +993,6 @@ public class ConcurrentHashTrieMap<K, V> extends AbstractMap<K, V> {
                 return new MultiSNode<K, V> (array);
             }
         }
-        
-        @Override
-        public KeyValueNode<K, V> first () {
-            return this;
-        }
 
         @Override
         public SNode<K, V> removed (final Object k) {
@@ -1186,15 +1069,6 @@ public class ConcurrentHashTrieMap<K, V> extends AbstractMap<K, V> {
                 }
             }
             return null;
-        }
-        
-        @Override
-        public KeyValueNode<K, V> first () {
-            if (this.content.length > 0) {
-                return this.content [0];
-            } else {
-                return null;
-            }
         }
 
         @Override
--- concurrent-hash-trie/test/lobstre/chtrie/TestLookupNext.java
+++ concurrent-hash-trie/test/lobstre/chtrie/TestLookupNext.java
@@ -1,20 +1,19 @@
 package lobstre.chtrie;
 
-import lobstre.chtrie.ConcurrentHashTrieMap.KeyValueNode;
 
 public class TestLookupNext {
     public static void main (final String[] args) {
-        final ConcurrentHashTrieMap<Object, Object> bt = new ConcurrentHashTrieMap<Object, Object> ();
-        TestHelper.assertTrue (null == bt.lookupNext (null));
-        for (int i = 0; i < 5000; i++) {
-            bt.put (Integer.valueOf (i), Integer.valueOf (i));
-            TestHelper.assertTrue (null != bt.lookupNext (null));
-            KeyValueNode<Object, Object> kvn = null;
-            int j = 0;
-            while (null != (kvn = bt.lookupNext (kvn))) {
-                j++;
-            }
-            TestHelper.assertEquals (i, j);
-        }
+//        final ConcurrentHashTrieMap<Object, Object> bt = new ConcurrentHashTrieMap<Object, Object> ();
+//        TestHelper.assertTrue (null == bt.lookupNext (null));
+//        for (int i = 0; i < 5000; i++) {
+//            bt.put (Integer.valueOf (i), Integer.valueOf (i));
+//            TestHelper.assertTrue (null != bt.lookupNext (null));
+//            KeyValueNode<Object, Object> kvn = null;
+//            int j = 0;
+//            while (null != (kvn = bt.lookupNext (kvn))) {
+//                j++;
+//            }
+//            TestHelper.assertEquals (i, j);
+//        }
     }
 }
