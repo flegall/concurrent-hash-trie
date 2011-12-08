e87792b2b2aa28e6a516ad7e9f3271b6022ebb2e
Florent Le Gall <florent.legall@gmail.com>
2011-12-08 18:10:12 +0100

    Added test for Iterator.remove ()

--- concurrent-hash-trie/test/lobstre/chtrie/TestMapIterator.java
+++ concurrent-hash-trie/test/lobstre/chtrie/TestMapIterator.java
@@ -1,11 +1,12 @@
 package lobstre.chtrie;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class TestMapIterator {
-    public static void main (String[] args) {
+    public static void main (final String[] args) {
         for (int i = 0; i < 5000; i++) {
             final Map<Integer, Integer> bt = new ConcurrentHashTrieMap<Integer, Integer> ();
             for (int j = 0; j < i; j++) {
@@ -23,9 +24,16 @@ public class TestMapIterator {
             for (final Integer j : bt.keySet ()) {
                 TestHelper.assertTrue (set.contains (j));
             }
-            
+
             TestHelper.assertEquals (i, count);
             TestHelper.assertEquals (i, bt.size ());
+
+            for (final Iterator<Integer> iter = bt.keySet ().iterator (); iter.hasNext ();) {
+                final Integer k = iter.next ();
+                TestHelper.assertTrue (bt.containsKey (k));
+                iter.remove ();
+                TestHelper.assertFalse (bt.containsKey (k));
+            }
         }
     }
 }
