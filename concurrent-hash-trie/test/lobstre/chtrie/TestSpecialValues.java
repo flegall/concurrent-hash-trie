ba0072028bbfc44697fba9453cb151efe64f4a0d
Florent Le Gall <florent.legall@gmail.com>
2011-12-29 14:27:12 +0100

    Added test for special values.

---
+++ concurrent-hash-trie/test/lobstre/chtrie/TestSpecialValues.java
@@ -0,0 +1,18 @@
+package lobstre.chtrie;
+
+import java.util.Map;
+
+public class TestSpecialValues {
+
+    public static void main (final String[] args) {
+        final Map<Object, Object> map = new ConcurrentHashTrieMap<Object, Object> ();
+        final Integer z3884 = Integer.valueOf (3884);
+        final Integer z4266 = Integer.valueOf (4266);
+        map.put (z3884, z3884);
+        TestHelper.assertTrue (null != map.get (z3884));
+        
+        map.put (z4266, z4266);
+        TestHelper.assertTrue (null != map.get (z3884));
+        TestHelper.assertTrue (null != map.get (z4266));
+    }
+}
