a9ba3164ade4b34f13ae1af3c99dbd6f22c01d74
Florent Le Gall <florent.legall@gmail.com>
2012-01-09 22:53:42 +0100

    Add shuffle to ensure the duplicates appear at any time.

--- concurrent-hash-trie/bench/lobstre/chtrie/BenchMapPut.java
+++ concurrent-hash-trie/bench/lobstre/chtrie/BenchMapPut.java
@@ -26,6 +26,7 @@ public class BenchMapPut {
                 objects.add (o);
             }
         }
+        Collections.shuffle (objects);
         
         final long begin = System.nanoTime ();
         for (final Object o : objects) {
